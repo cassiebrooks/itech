93459cf518e0b7e78e1065de7d81c8dd83cba1a4
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 15:06:38 +0000

    Added UserProfile model & migrated

--- quokka/admin.py
+++ quokka/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from quokka.models import Question, Set, Answer, Votes
+from quokka.models import Question, Set, Answer, Votes, UserProfile
 
 admin.site.register(Question)
 admin.site.register(Set)
 admin.site.register(Answer)
 admin.site.register(Votes)
+admin.site.register(UserProfile)
--- quokka/models.py
+++ quokka/models.py
@@ -1,5 +1,6 @@
 # from __future__ import unicode_literals
 from django.db import models
+from django.contrib.auth.models import User
 
 class Set(models.Model):
 	id = models.AutoField(primary_key=True)
@@ -30,3 +31,12 @@ class Votes(models.Model):
 	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 	#voter = models.ForeignKey(User, on_delete=models.CASCADE)
 	score = models.IntegerField()
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    score = models.IntegerField(default=0)
+
+    # Override the __unicode__() method to return out something meaningful!
+    def __unicode__(self):
+        return self.user.username
