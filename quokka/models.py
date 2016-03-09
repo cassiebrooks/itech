5fa32b5bcc3b691c3fb9c1fc10b47f6aa7c3d80b
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:12:26 +0000

    Add Thomas' models

--- quokka/models.py
+++ quokka/models.py
@@ -1,3 +1,32 @@
+# from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Set(models.Model):
+	id = models.AutoField(primary_key=True)
+	name = models.CharField(max_length=128, unique=True)
+	def __unicode__(self):
+		return unicode(self.name)
+
+class Question(models.Model):
+	id = models.AutoField(primary_key=True)
+	text = models.CharField(max_length=512)
+	def __unicode__(self):
+		return unicode(self.id)
+
+# class User(models.Model):
+# 	username = models.CharField(max_length=32, unique=True)
+# 	def __unicode__(self):
+# 		return unicode(self.username)
+
+class Answer(models.Model):
+	id = models.AutoField(primary_key=True)
+	text = models.CharField(max_length=1024)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	#author = models.ForeignKey(User, on_delete=models.CASCADE)
+	def __unicode__(self):
+		return unicode(self.id)
+
+class Votes(models.Model):
+	answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+	#voter = models.ForeignKey(User, on_delete=models.CASCADE)
+	score = models.IntegerField()
