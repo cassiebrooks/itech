7c55cbea318a49f38c8dde7e50b8e3958402df0d
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:22:08 +0000

    Get all questions, etc. from DB in index.html

--- quokka/views.py
+++ quokka/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from quokka.models import Question, Answer, Votes, Set
 
 def index(request):
-    context_dict = {'boldmessage': "I am bold font from the context"}
+    question_list = Question.objects.all()
+    answer_list = Answer.objects.all()
+    votes_list = Votes.objects.all()
+    context_dict = {'questions': question_list, 'answers': answer_list, 'votes': votes_list}
     return render(request, 'quokka/index.html', context_dict)
 
 def about(request):
