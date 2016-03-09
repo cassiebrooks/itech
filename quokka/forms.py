5bde647463e32f5f7e6b2b43f151f1a88984a326
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:52:30 +0000

    Working submit answer form (on separate page)

--- quokka/forms.py
+++ quokka/forms.py
@@ -3,8 +3,21 @@ from quokka.models import Question, Answer, Votes, Set
 
 
 class AnswerForm(forms.ModelForm):
+    question = forms.ModelChoiceField(queryset=Question.objects.all())
     text = forms.CharField(max_length=512)
+
     class Meta:
         # Provide an association between the ModelForm and a model
         model   = Answer
-        fields  = ('text',)
+        fields  = ('text', 'question')
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with 'http://', prepend 'http://'.
+        if url and not url.startswith('http://'):
+            url = 'http://' + url
+            cleaned_data['url'] = url
+
+        return cleaned_data
--- quokka/views.py
+++ quokka/views.py
@@ -15,25 +15,13 @@ def about(request):
     return render(request, 'quokka/about.html', context_dict)
 
 def add_answer(request):
-    # A HTTP POST?
     if request.method == 'POST':
         form = AnswerForm(request.POST)
-
-        # Have we been provided with a valid form?
         if form.is_valid():
-            # Save the new category to the database.
-            form.save(commit=True)
-
-            # Now call the index() view.
-            # The user will be shown the homepage.
-            return index(request)
+        	form.save(commit=True)
+        	return index(request)
         else:
-            # The supplied form contained errors - just print them to the terminal.
             print form.errors
     else:
-        # If the request was not a POST, display the form to enter details.
         form = AnswerForm()
-
-    # Bad form (or form details), no form supplied...
-    # Render the form with error messages (if any).
     return render(request, 'quokka/add_answer.html', {'form': form})
