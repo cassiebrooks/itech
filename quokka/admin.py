bb92b63858e781b5e7b5271e82477d7c17294fdc
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:17:43 +0000

    Register our models with Django Admin

--- quokka/admin.py
+++ quokka/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from quokka.models import Question, Set, Answer, Votes
 
-# Register your models here.
+admin.site.register(Question)
+admin.site.register(Set)
+admin.site.register(Answer)
+admin.site.register(Votes)
