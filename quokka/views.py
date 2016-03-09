c58ba833e91c1f5bcd89507e0f8eba548f4747b4
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 13:58:49 +0000

    Add simple template folder and index.html

--- quokka/views.py
+++ quokka/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Quokka says hey there world!")
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render(request, 'quokka/index.html', context_dict)
 
 def about(request):
     return HttpResponse("Quokka tells you about itself!")
