30677ebecc2638924e0768d322338921ed0d054c
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 16:21:31 +0000

    Add cookie to count user visits (not working?)

--- quokka/views.py
+++ quokka/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from datetime import datetime
 from quokka.models import Question, Answer, Votes, Set
 from quokka.forms import AnswerForm, UserForm, UserProfileForm
 
@@ -10,11 +11,36 @@ def index(request):
     answer_list = Answer.objects.all()
     votes_list = Votes.objects.all()
     context_dict = {'questions': question_list, 'answers': answer_list, 'votes': votes_list}
-    return render(request, 'quokka/index.html', context_dict)
+    response = render(request, 'quokka/index.html', context_dict)
+
+    # Cookies
+    visits = int(request.COOKIES.get('visits', '1'))
+    reset_last_visit_time = False
+
+    if 'last_visit' in request.COOKIES:
+        last_visit = request.COOKIES['last_visit']
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        if (datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+        context_dict['visits'] = visits
+        response = render(request, 'quokka/index.html', context_dict)
+
+    if reset_last_visit_time:
+        response.set_cookie('last_visit', datetime.now())
+        response.set_cookie('visits', visits)
+
+    return response
+
 
 def about(request):
-    context_dict = {'boldmessage': "YAY QUOKKA"}
-    return render(request, 'quokka/about.html', context_dict)
+	if request.session.get('visits'):
+	    count = request.session.get('visits')
+	else:
+	    count = 0
+	return render(request, 'quokka/about.html', {'visits': count})
 
 @login_required
 def add_answer(request):
