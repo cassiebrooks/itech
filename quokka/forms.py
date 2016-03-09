8435f25ffbe243afc0466216d049905dd26ead3f
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 15:38:30 +0000

    Add login functionality too

--- quokka/forms.py
+++ quokka/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from quokka.models import Question, Answer, Votes, Set
+from django.contrib.auth.models import User
+from quokka.models import Question, Answer, Votes, Set, UserProfile
 
 
 class AnswerForm(forms.ModelForm):
@@ -21,3 +22,17 @@ class AnswerForm(forms.ModelForm):
             cleaned_data['url'] = url
 
         return cleaned_data
+
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('picture',)
--- quokka/urls.py
+++ quokka/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^about/', views.about, name='about'),
 	url(r'^add_answer/$', views.add_answer, name='add_answer'),
+	url(r'^register/$', views.register, name='register'),
+	url(r'^login/$', views.user_login, name='login'),
 ]
