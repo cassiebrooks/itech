1a854a09e420d047b128958a4bb063fe16b17ceb
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 13:54:28 +0000

    Added basic about page

--- quokka/views.py
+++ quokka/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Quokka says hey there world!")
+
+def about(request):
+    return HttpResponse("Quokka tells you about itself!")
