7d4cd337a56b21a4531d90050e85edc602ae3304
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 18:14:13 +0000

    Correct registration redirect error

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -2,10 +2,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from registration.backends.simple.views import RegistrationView
+
+class MyRegistrationView(RegistrationView):
+    def get_success_url(self, user):
+        return '/quokka/'
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^quokka/', include('quokka.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
