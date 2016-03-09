1f770b1911e256f9cd0b343fe172eb8cfe5e2ba1
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:19:05 +0000

    Added populate script

---
+++ populate_quokka.py
@@ -0,0 +1,44 @@
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
+
+import django
+django.setup()
+
+from quokka.models import Set, Question, Answer, Votes#, User
+
+
+def populate():
+
+    # Print out what we have added to the user.
+    # for c in Category.objects.all():
+    #     for p in Page.objects.filter(category=c):
+    #         print "- {0} - {1}".format(str(c), str(p))
+
+    my_set = add_set("My Set")
+
+    add_question("What is your name?")
+    add_question("What is your favourite colour?")
+    add_question("What is the average airspeed velocity of an unladen swallow?")
+
+    add_answer("My name is Quokka")
+    add_answer("My name is Azzopardi")
+
+def add_question(text):
+    q, created = Question.objects.get_or_create(text=text)
+    q.save()
+    return q
+
+def add_set(name):
+    s = Set.objects.get_or_create(name=name)[0]
+    s.save()
+    return s
+
+def add_answer(text): # NOTE: assigns the answer to a random question
+    a = Answer.objects.get_or_create(text=text, question=Question.objects.order_by('?').first())[0]
+    return a
+
+# Start execution here!
+if __name__ == '__main__':
+    print "Starting Quokka population script..."
+    populate()
+    print "Populated, no errors!\n"
