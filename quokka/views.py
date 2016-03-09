5e572456da35d926bb90f794e27fc0c5359cc7bf
cassiebrooks <cassie.brooks@af83.com>
2016-03-09 14:11:21 +0000

    Make About page into template instead of HttpResponse

--- quokka/views.py
+++ quokka/views.py
@@ -6,4 +6,5 @@ def index(request):
     return render(request, 'quokka/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Quokka tells you about itself!")
+    context_dict = {'boldmessage': "YAY QUOKKA"}
+    return render(request, 'quokka/about.html', context_dict)
