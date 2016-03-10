9698b2a04c837d0eecfb9835a6a5fd1f7d2a1378
cassiebrooks <cassie.brooks@af83.com>
2016-03-10 12:31:49 +0000

    Whitespace

--- static/js/quokka-ajax.js
+++ static/js/quokka-ajax.js
@@ -1,25 +1,19 @@
-console.log("entered quokka-ajeeeeex file");
-
 $('.up').click(function(){
-	console.log("entered function on 'up' click");
-	console.log(this);
     var aid = $(this).attr("data-aid");
     var score = 1;
     $.get('/quokka/vote_on_answer/', {answer_id: aid, score: score}, function(data){
-				var current_score = $('#a-'+aid+' .score').html();
-				$('#a-'+aid+' .score').html(parseInt(current_score)+1)
-				$('#up').hide();
+		var current_score = $('#a-'+aid+' .score').html();
+		$('#a-'+aid+' .score').html(parseInt(current_score)+1)
+		$('#up').hide();
     });
 });
 
 $('.down').click(function(){
-	console.log("entered function on 'down' click");
-	console.log(this);
     var aid = $(this).attr("data-aid");
     var score = -1
     $.get('/quokka/vote_on_answer/', {answer_id: aid, score: score}, function(data){
-               var current_score = $('#a-'+aid+' .score').html();
-				$('#a-'+aid+' .score').html(parseInt(current_score)-1)
-				$('#down').hide();
+       var current_score = $('#a-'+aid+' .score').html();
+		$('#a-'+aid+' .score').html(parseInt(current_score)-1)
+		$('#down').hide();
     });
 });
