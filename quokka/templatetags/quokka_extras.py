d243e96bfc343792dac781f37a8b772b36babe3b
cassiebrooks <cassie.brooks@af83.com>
2016-03-11 21:21:38 +0000

    Add (non-functioning but pretty) form if User not answered Answer

--- quokka/templatetags/quokka_extras.py
+++ quokka/templatetags/quokka_extras.py
@@ -14,3 +14,36 @@ def get_vote_count(answer_id, votes):
 			elif vote.score == -1:
 				count -= 1
 	return count
+
+
+@register.simple_tag
+def user_has_answered (question, user, answers):
+# if user has already answered the question:
+	for answer in answers:
+		if answer.question.id == question.id:
+			if answer.author.id == user.id:
+				return 1
+	return 0
+
+
+# @register.simple_tag
+# def display_answer_form(answer_id, votes):
+# 	# returns html of answer form ?
+# 	return "<div class='card'>[form]</div>"
+
+# @register.simple_tag
+# def highlight_answer(answer_id):
+# 	# somehow highlights the user's answer within the answer list
+# 	return -1
+
+
+
+
+# # within 'for answer in answers'
+# if answer.author.id == user.id:
+#     user_replied = True
+#     highlight_answer(answer.id) # highlight user's own answer
+
+# # after for loop:
+# if !user_replied:
+#     insert question answer form at top of list
