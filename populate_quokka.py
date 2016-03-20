23c1d616ee2f4cc622de2a48d0f41fd600373741
thomas-toussaint <2028329t@student.glasgow.ac.uk>
2016-03-20 21:47:58 +0000

    Added the questions to the population script

--- populate_quokka.py
+++ populate_quokka.py
@@ -17,10 +17,49 @@ def populate():
     #         print "- {0} - {1}".format(str(c), str(p))
 
     my_set = add_set("My Set")
+    set_1 = add_set("Set I")
+    set_2 = add_set("Set II")
+    set_3= add_set("Set III")
+    add_question("Given the choice of anyone in the world, whom would you want as a dinner guest?", set=set_1)
+    add_question("Would you like to be famous? In what way?", set=set_1)
+    add_question("Before making a telephone call, do you ever rehearse what you are going to say? Why?", set=set_1)
+    add_question("What would constitute a 'perfect' day for you?", set=set_1)
+    add_question("What did you last sing to yourself? To someone else?", set=set_1)
+    add_question("If you were able to live to the age of 90 and retain either the mind or body of a 30-year-old for"+
+                 "the last 60 years of your life, which would you want?",set=set_1)
+    add_question("Do you have a secret hunch about how you will die?",set=set_1)
+    add_question("Name three things you and your partner appear to have in common.",set=set_1)
+    add_question("For what in your life do you feel most grateful?",set=set_1)
+    add_question("If you could change anything about the way you were raised, what would it be?",set=set_1)
+    add_question("Take four minutes and tell your life story in as much detail as possible.",set=set_1)
+    add_question("If you could wake up tomorrow having gained any one quality or ability, what would it be?", set=set_1)
+    add_question("If a crystal ball could tell you the truth about yourself, your life, "+
+                 "the future or anything else, what would you want to know?",set=set_2 )
+    add_question("Is there something that you’ve dreamed of doing for a long time? Why haven’t you done it?", set=set_2)
+    add_question("What is the greatest accomplishment of your life?",set=set_2)
+    add_question("What do you value most in a friendship?",set=set_2)
+    add_question("What is your most treasured memory?",set=set_2)
+    add_question("What is your most terrible memory?",set=set_2)
+    add_question("If you knew that in one year you would die suddenly, would you change anything about the way"+
+                 " you are now living? Why?",set=set_2)
+    add_question("What does friendship mean to you?",set=set_2)
+    add_question("What roles do love and affection play in your life?",set=set_2)
+    add_question("How close and warm is your family? Do you feel your childhood was happier than most other people's?",set=set_2)
+    add_question("How do you feel about your relationship with your mother?",set=set_2)
+    add_question("Share an embarrassing moment of your life",set=set_3)
+    add_question("When did you last cry in front of another person?",set=set_3)
+    add_question("What, if anything, is too serious to be joked about?",set=set_3)
+    add_question("If you were to die this evening with no opportunity to communicate with anyone, "+
+                 "what would you most regret not having told someone? Why haven’t you told them yet?",set=set_3)
+    add_question("Your house, containing everything you own, catches fire. After saving your loved ones and pets, you"+
+                 " have time to safely make a final dash to save any one item. What would it be? Why?",set=set_3)
+    add_question("Of all the people in your family,w hose death would you find most disturbing? Why?",set=set_3)
 
-    add_question("What is your name?")
-    add_question("What is your favourite colour?")
-    add_question("What is the average airspeed velocity of an unladen swallow?")
+
+
+    #add_question("What is your name?")
+    #add_question("What is your favourite colour?")
+    #add_question("What is the average airspeed velocity of an unladen swallow?")
     add_user("leifos", "leifos")
     add_user("laura", "laura")
     add_user("david", "david")
@@ -35,8 +74,8 @@ def add_user(name, pw):
     return u
 
 
-def add_question(text):
-    q, created = Question.objects.get_or_create(text=text)
+def add_question(text, qset=None):
+    q, created = Question.objects.get_or_create(text=text, set=qset)
     q.save()
     return q
 
